033ed3586d1e8d57d10574ee9670d6d68e471b54
nathan-siu1234567 <nathansiu9730@gmail.com>
2016-05-12 19:33:36 -0400

    test2

---
+++ COMP2007-S2016-LESSON1C/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP2007_S2016_LESSON1C
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            FullNameBox.Text = FirstNameTextBox.Text + TextBox1.Text;
+        }
+       
+
+       
+    }
+}
